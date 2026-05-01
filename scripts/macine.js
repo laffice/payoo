591a866adf944461a1bbc0f0e5f2f3928cc31a7b
laffice <lafficehossen212@gmail.com>
2026-05-01 18:10:34 +0600

    buttons are working

---
+++ scripts/macine.js
@@ -0,0 +1,21 @@
+// console.log('machine js file is connected')
+
+function showOnly(id){
+    
+    const addMoney= document.getElementById('add-money');
+    const cashOut= document.getElementById('cash-out');
+    const transferMoney= document.getElementById('transfer-money');
+    const payBills= document.getElementById('pay-bills');
+    const getBonus= document.getElementById('get-bonus');
+    const transactions= document.getElementById('transactions');
+
+    addMoney.classList.add('hidden');
+    cashOut.classList.add('hidden');
+    transferMoney.classList.add('hidden');
+    payBills.classList.add('hidden');
+    getBonus.classList.add('hidden');
+
+
+    const selected= document.getElementById(id);
+    selected.classList.remove('hidden');
+}
